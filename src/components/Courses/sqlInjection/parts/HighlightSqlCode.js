8b8ae2210c42af6f7a7706f45cc253a0af83758a
Ben <rikenpl99@gmail.com>
2021-11-21 16:56:50 +0100

    changed from codemirror to ace editor

--- src/components/Courses/sqlInjection/parts/HighlightSqlCode.js
+++ src/components/Courses/sqlInjection/parts/HighlightSqlCode.js
@@ -1,73 +1,81 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect, useRef } from "react";
 import styled from "styled-components";
-import Browser from "../../Browser/Browser.js"
+import Browser from "../../Browser/Browser.js";
 
+import AceEditor from "react-ace";
 
-import CodeMirror from '@uiw/react-codemirror';
+import "ace-builds/src-noconflict/mode-java";
+import "ace-builds/src-noconflict/theme-dracula";
 
 const Container = styled.div`
-width: 100%;
-height: 100%;
-display: grid;
-grid-template-columns: 1fr 1fr;
-`
+  width: 100%;
+  height: 100%;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+`;
 
 const Wrapper = styled.div`
-padding: 25px;
-
-`
+  padding: 25px;
+`;
 
 const PageBody = styled.div`
-width: 100%;
-height: 250px;
-display: flex;
-flex-direction: column;
-align-items: center;
-`
-
-
+  width: 100%;
+  height: 250px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 const SqlInteractive = (props) => {
-
-
   useEffect(() => {
-    props.setWaitForCorrectAnswer(true)
-},[])
+    props.setWaitForCorrectAnswer(true);
+  }, []);
 
+  const aceEditor = useRef(null);
 
-const getSelectedText = () => {
-  if (window.getSelection){
-  const highlitedText = window.getSelection().toString()
-  if (highlitedText === 'FROM') {
-    props.setWaitForCorrectAnswer(false)
-  } else {
-    props.setWaitForCorrectAnswer(true)
-  }
-  }
-}
+  const getSelectedText = () => {
+    if (aceEditor.current.editor.getSelectedText().includes("FROM")) {
+      props.setWaitForCorrectAnswer(false);
+    } else {
+      props.setWaitForCorrectAnswer(true);
+    }
+  };
   const code = `const a = 0;
 const b = 1;
 const sql = "SELECT * FROM products p where p.name = ''"
 `;
-    return (
-        <Container>
-            <Wrapper>
-                <Browser>
-                <PageBody>
-                    <h1>Sklep internetowy</h1>
-                    <label for="input">Wpisz nazwę produktu aby wyszukać:</label>
-                    <input name="input" disabled/>
-                    <br/>
-                    <button disabled>Wyszukaj</button>
-                </PageBody>
-                </Browser>
-            </Wrapper>
-<Wrapper><CodeMirror
-  value={code}
-  onMouseUp={getSelectedText}
-/></Wrapper>
-        </Container>
-    )
-}
 
-export default SqlInteractive
+  return (
+    <Container>
+      <Wrapper>
+        <Browser>
+          <PageBody>
+            <h1>Sklep internetowy</h1>
+            <label for="input">Wpisz nazwę produktu aby wyszukać:</label>
+            <input name="input" disabled />
+            <br />
+            <button disabled>Wyszukaj</button>
+          </PageBody>
+        </Browser>
+      </Wrapper>
+      <Wrapper>
+        {/* <CodeMirror
+          value={code}
+          extensions={[javascript({ jsx: true })]}
+        /> */}
+        <AceEditor
+          ref={aceEditor}
+          mode="java"
+          theme="dracula"
+          value={code}
+          fontSize={16}
+          wrapEnabled={true}
+          onCursorChange={getSelectedText}
+          readOnly
+        />
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default SqlInteractive;
--- src/components/Courses/sqlInjection/parts/SqlInteractive.js
+++ src/components/Courses/sqlInjection/parts/SqlInteractive.js
@@ -1,67 +1,79 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-import Browser from "../../Browser/Browser.js"
+import Browser from "../../Browser/Browser.js";
 
-
-import CodeMirror from '@uiw/react-codemirror';
+import AceEditor from "react-ace";
+import "ace-builds/src-noconflict/mode-java";
+import "ace-builds/src-noconflict/theme-dracula";
 
 const Container = styled.div`
-width: 100%;
-height: 100%;
-display: grid;
-grid-template-columns: 1fr 1fr;
-`
+  width: 100%;
+  height: 100%;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+`;
 
 const Wrapper = styled.div`
-padding: 25px;
-
-`
+  padding: 25px;
+`;
 
 const PageBody = styled.div`
-width: 100%;
-height: 250px;
-display: flex;
-flex-direction: column;
-align-items: center;
-`
-
-
+  width: 100%;
+  height: 250px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 const SqlInteractive = (props) => {
-
   const [search, setSearch] = useState("");
 
   useEffect(() => {
-    props.setWaitForCorrectAnswer(true)
-},[])
+    props.setWaitForCorrectAnswer(true);
+  }, []);
 
   const code = `const a = 0;
 const b = 1;
 const sql = "SELECT * FROM products p where p.name = '${search}'"
 `;
-    return (
-        <Container>
-            <Wrapper>
-                <Browser>
-                <PageBody>
-                    <h1>Sklep internetowy</h1>
-                    <label for="input">Wpisz nazwę produktu aby wyszukać:</label>
-                    <input name="input" onChange={(e) => {
-                      setSearch(e.target.value)
-                      }}/>
-                    <br/>
-                    <button onClick={() => {
-                      if(search.match(/' or \d+=\d+ --/gi)) props.setWaitForCorrectAnswer(false)
-                      else props.setWaitForCorrectAnswer(true)
-                    }}>Wyszukaj</button>
-                </PageBody>
-                </Browser>
-            </Wrapper>
-<Wrapper><CodeMirror
-  value={code}
-/></Wrapper>
-        </Container>
-    )
-}
+  return (
+    <Container>
+      <Wrapper>
+        <Browser>
+          <PageBody>
+            <h1>Sklep internetowy</h1>
+            <label for="input">Wpisz nazwę produktu aby wyszukać:</label>
+            <input
+              name="input"
+              onChange={(e) => {
+                setSearch(e.target.value);
+              }}
+            />
+            <br />
+            <button
+              onClick={() => {
+                if (search.match(/' or \d+=\d+ --/gi))
+                  props.setWaitForCorrectAnswer(false);
+                else props.setWaitForCorrectAnswer(true);
+              }}
+            >
+              Wyszukaj
+            </button>
+          </PageBody>
+        </Browser>
+      </Wrapper>
+      <Wrapper>
+      <AceEditor
+            mode="java"
+            theme="dracula"
+            value={code}
+            fontSize={16}
+            wrapEnabled={true}
+            readOnly
+        />
+      </Wrapper>
+    </Container>
+  );
+};
 
-export default SqlInteractive
+export default SqlInteractive;

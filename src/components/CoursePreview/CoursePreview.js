99a341af3020e0f8a0d5aebaad3a3aa79a6fa14e
ben <benmotykax@gmail.com>
2022-02-25 00:02:11 +0100

    added basic course preview styles and structure

--- src/components/CoursePreview/CoursePreview.js
+++ src/components/CoursePreview/CoursePreview.js
@@ -1,18 +1,73 @@
-import {Container, ReviewWrapper, ContentWrapper, RateWrapper, CommentsWrapper, Comment } from "./CoursePreview.styles"
+import Button from "components/Button/Button";
+import {
+  Container,
+  ReviewWrapper,
+  ContentWrapper,
+  RateWrapper,
+  CommentsWrapper,
+  Comment,
+  Header,
+  Description,
+  ContentHeaderWrapper,
+  CourseTitlesWrapper,
+  CourseTitle,
+  ButtonWrapper,
+  RateLevel,
+  EmptyRateLevel,
+  RateDescription,
+  RateSubDescription,
+  CommentsHeader,
+  CommentsContainer,
+} from "./CoursePreview.styles";
 
 const CoursePreview = () => {
   return (
     <Container>
-        <ContentWrapper>content</ContentWrapper>
-        <ReviewWrapper>
-            <RateWrapper>5 gwiazdek</RateWrapper>
-            <CommentsWrapper>
-                <Comment>heja</Comment>
-                <Comment>supera</Comment>
-            </CommentsWrapper>
-        </ReviewWrapper>
-    </Container>
-  )
-}
+      <ContentWrapper>
+        <ContentHeaderWrapper>
+          <Header>Kurs ransomware</Header>
+          <Description>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit
+            amet risus libero. Phasellus dignissim mi quis libero congue, vel
+            placerat nisi egestas. Duis pellentesque leo ut eros suscipit
+            scelerisque. Pellentesque venenatis fermentum libero in mattis.
+            Vestibulum a accumsan libero. Integer finibus condimentum tempus.
+            <br />
+            <br />W tym kursie dowiesz się:
+          </Description>
+          <CourseTitlesWrapper>
+            <CourseTitle>Czym jest ransomware?</CourseTitle>
+            <CourseTitle>Rodzaje i typy ranwomware</CourseTitle>
+            <CourseTitle>Jak się bronić?</CourseTitle>
+          </CourseTitlesWrapper>
+        </ContentHeaderWrapper>
+        <ButtonWrapper>
+          <Button text="ROZPOCZNIJ" />
+        </ButtonWrapper>
+      </ContentWrapper>
+      <ReviewWrapper>
+        <RateWrapper>
+          <RateLevel />
+          <RateLevel />
+          <RateLevel />
+          <RateLevel />
+          <EmptyRateLevel />
+        </RateWrapper>
+        <RateDescription>Ocena: 4/5</RateDescription>
+        <RateSubDescription>17 głosów</RateSubDescription>
+        <CommentsContainer>
+          <CommentsHeader>Komentarze</CommentsHeader>
+          <CommentsWrapper>
+            <Comment>komentarz</Comment>
+            <Comment>komentarz</Comment>
+            <Comment>komentarz</Comment>
+            <Comment>komentarz</Comment>
 
-export default CoursePreview
+          </CommentsWrapper>
+        </CommentsContainer>
+      </ReviewWrapper>
+    </Container>
+  );
+};
+
+export default CoursePreview;
--- src/components/CoursePreview/CoursePreview.styles.js
+++ src/components/CoursePreview/CoursePreview.styles.js
@@ -1,41 +1,127 @@
 import styled, {css} from "styled-components";
 import colors from "constans/colors"
+import { RateLevel as Rate, EmptyRateLevel as EmptyRate } from "components/PreviewItems/Previews/Previews.styles";
 
 export const Container = styled.div`
 display: grid;
-grid-template-columns: 75% 25%;
-width: 100%;
-height: 100%;
-background-color: red;
+grid-template-columns: 65% 35%;
+grid-template-rows: 1fr;
+width: 60%;
+height: 90%;
+padding: 50px 35px;
+box-sizing: border-box;
+background-color: ${colors.mediumDarkPurple};
 `
 
 export const ContentWrapper = styled.div`
-background-color: yellow;
 display: flex;
-align-items: center;
 flex-direction: column;
+height: 100%;
+`
+
+export const ContentHeaderWrapper = styled.div`
+width: 85%;
+`
+
+export const Header = styled.h2`
+margin: 0;
+color: ${colors.orange};
+font-size: 42px;
+opacity: 0.9;
+`
+
+export const Description = styled.p`
+font-size: 16px;
+color: ${colors.white};
+opacity: 0.7;
+`
+
+export const CourseTitlesWrapper = styled.ul`
+padding-left: 25px;
+`
+
+export const CourseTitle = styled.li`
+color: ${colors.white};
+font-size: 16px;
+opacity: 0.7;
 `
 
 export const ReviewWrapper = styled.div`
-background-color: white;
 display: flex;
 flex-direction: column;
+height: 100%;
 `
 
 export const RateWrapper = styled.div`
 margin: 0;
 display: grid;
 grid-template-columns: repeat(5, 1fr);
+text-align: center;
+`
+
+export const CommentsContainer = styled.div`
+margin: 20px 0 0 0;
+height: 100%;
+background-color: ${colors.darkPurple};
 `
 
 export const CommentsWrapper = styled.ul`
-background-color: green;
 list-style: none;
 padding: 0;
-margin: 0;
+overflow-y: auto;
+height: 100%;
 `
 
 export const Comment = styled.li`
-background-color: blue;
 width: 100%;
+border: 1px solid ${colors.darkestPurple};
+box-sizing: border-box;
+font-size: 14px;
+color: ${colors.white};
+opacity: 0.7;
+border-top: none;
+`
+
+export const CommentsHeader = styled.h4`
+width: 100%;
+font-size: 24px;
+text-align: center;
+background-color: ${colors.darkestPurple};
+color: ${colors.orange};
+padding: 10px 0;
+margin: 0;
+`
+
+export const ButtonWrapper = styled.div`
+margin: auto auto 0;
+`
+
+const Star = css`
+font-size: 50px;
+`
+
+export const RateLevel = styled(Rate)`
+${Star};
+`
+export const EmptyRateLevel = styled(EmptyRate)`
+${Star};
+`
+
+const GoldText = css`
+opacity: 0.6;
+color: ${colors.gold};
+text-align: center;
+
+`
+
+export const RateDescription = styled.h4`
+${GoldText};
+font-size: 24px;
+margin: 0;
+`
+
+export const RateSubDescription = styled.p`
+${GoldText};
+font-size: 12px;
+margin: 0;
 `

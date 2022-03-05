7b14d872e2ed98505cc5691d30126f02af1b178f
Ben <benmotykax@gmail.com>
2022-03-05 19:25:02 +0100

    removed rating stars and course preview on course preview items

--- src/components/PreviewItems/Previews/Course.js
+++ src/components/PreviewItems/Previews/Course.js
@@ -1,34 +1,58 @@
-import React from 'react'
-import Parser from 'html-react-parser';
+import React from "react";
+import Parser from "html-react-parser";
 
-import {Container, ImageContainer,Image, Body, HeaderContainer, Header, Arrow, Description, RateLevel, DifficultyWrapper, FooterContainer, Comment, CommentWrapper, EmptyRateLevel } from "./Previews.styles"
-import Difficulty from './Difficulty/Difficulty';
+import {
+  Container,
+  ImageContainer,
+  Image,
+  Body,
+  HeaderContainer,
+  Header,
+  Arrow,
+  Description,
+  RateLevel,
+  DifficultyWrapper,
+  FooterContainer,
+  Comment,
+  CommentWrapper,
+  EmptyRateLevel,
+} from "./Previews.styles";
+import Difficulty from "./Difficulty/Difficulty";
 const Course = (props) => {
-    return (
-<Container to={props.to}>
-<ImageContainer>
-    <Image src={props.img}/>
-</ImageContainer>
-    <Body>
+  return (
+    <Container to={props.to}>
+      <ImageContainer>
+        <Image src={props.img} />
+      </ImageContainer>
+      <Body>
         <HeaderContainer>
-            <Header>{props.header}{<Difficulty level={props.difficulty}/>}</Header>
-            <Arrow/>
+          <Header>
+            {props.header}
+            {<Difficulty level={props.difficulty} />}
+          </Header>
+          <Arrow />
         </HeaderContainer>
         <Description>{Parser(props.description)}</Description>
 
-    <FooterContainer><CommentWrapper>51<Comment/></CommentWrapper> <div>
-    <DifficultyWrapper>
-    {[...Array(props.difficulty)].map((level, index) => (
-        <RateLevel key={index}/>
-    ))} 
-    {[...Array(5 - props.difficulty)].map((level, index) => (
-        <EmptyRateLevel key={index}/>
-    ))} 
-    </DifficultyWrapper>
-        </div></FooterContainer>
-    </Body>
-</Container>            
-    )
-}
+        {/* <FooterContainer>
+          <CommentWrapper>
+            51
+            <Comment />
+          </CommentWrapper>
+          <div>
+            <DifficultyWrapper>
+              {[...Array(props.difficulty)].map((level, index) => (
+                <RateLevel key={index} />
+              ))}
+              {[...Array(5 - props.difficulty)].map((level, index) => (
+                <EmptyRateLevel key={index} />
+              ))}
+            </DifficultyWrapper>
+          </div>
+        </FooterContainer> */}
+      </Body>
+    </Container>
+  );
+};
 
-export default Course
+export default Course;

6788f949d7513a5477e15e364d41b00c43909bd6
Ben <benmotykax@gmail.com>
2022-03-18 23:21:32 +0100

    changed displaying articles, added new features

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -1,14 +1,47 @@
-import {Container, Header, Line, Content, Link} from "./Article.styles"
-import Parser from 'html-react-parser';
+import {
+  Container,
+  Header,
+  Line,
+  Content,
+  Wrapper,
+  AdditionalInfoContainer,
+  AdditionalInfoWrapper,
+  CategoriesWrapper,
+  UrlLink,
+  Category,
+  SectionHeader,
+} from "./Article.styles";
+import Parser from "html-react-parser";
+import { useTranslation } from "react-i18next";
 
 const Article = (props) => {
-    return (
-        <Container>
-            <Header>{props.name}</Header>
-            <Line/>
-          <Content>{Parser(props.description)}</Content>
-        </Container >
-    )
-}
+  const {t} = useTranslation()
 
-export default Article
+  return (
+    <Container>
+      <Wrapper>
+        <Header>{props.name}</Header>
+        <Line />
+        <Content>{Parser(props.description)}</Content>
+      </Wrapper>
+      <AdditionalInfoContainer>
+        <AdditionalInfoWrapper>
+          <SectionHeader>{t('usefulLinks')}</SectionHeader>
+          <Line smallMargin />
+          <UrlLink target="_blank" href="https://wikipedia.org">
+            https://wikipedia.org
+          </UrlLink>
+        </AdditionalInfoWrapper>
+        <AdditionalInfoWrapper>
+          <SectionHeader>{t('categories')}</SectionHeader>
+          <Line smallMargin />
+          <CategoriesWrapper flex>
+            <Category>Privacy</Category>
+          </CategoriesWrapper>
+        </AdditionalInfoWrapper>
+      </AdditionalInfoContainer>
+    </Container>
+  );
+};
+
+export default Article;
--- src/components/Article/Article.styles.js
+++ src/components/Article/Article.styles.js
@@ -1,29 +1,69 @@
-import styled from "styled-components";
-import colors from "constans/colors"
+import styled, { css } from "styled-components";
+import colors from "constans/colors";
 
 export const Container = styled.div`
-width: 100%;
-max-width: 1200px;
-margin-bottom: 80px;
-min-height: 200px;
+  width: 100%;
+  max-width: 1200px;
+  margin-bottom: 80px;
+  min-height: 200px;
+  padding: 25px;
+  display: grid;
+  grid-template-columns: 65% 35%;
+`;
 
-    padding: 25px;
-`
+export const Wrapper = styled.main``;
 export const Header = styled.h2`
-color: ${colors.white};
-font-size: 26px;
-margin: 0;
-`
+  color: ${colors.white};
+  font-size: 26px;
+  margin: 0;
+`;
 
 export const Line = styled.hr`
-margin: 35px 0;
-opacity: 10%;
-`
+  opacity: 10%;
+  margin: ${(props) => (props.smallMargin ? "15px 0" : "35px 0")};
+`;
 export const Content = styled.p`
-color: ${colors.white};
-opacity: 0.7;
-`
+  color: ${colors.white};
+  opacity: 0.7;
+`;
 
 export const Link = styled.p`
-font-size: 50px;
-`
+  font-size: 50px;
+`;
+
+export const AdditionalInfoContainer = styled.div`
+  margin: 75px 35px;
+`;
+
+export const AdditionalInfoWrapper = styled.div`
+  background-color: ${colors.darkPurple};
+  border-radius: 5px;
+  margin: 0;
+  margin-bottom: 30px;
+  padding: 25px;
+`;
+
+export const CategoriesWrapper = styled.div`
+  display: flex;
+  gap: 15px 20px;
+  flex-wrap: wrap;
+`;
+
+export const UrlLink = styled.a`
+  color: ${colors.white};
+`;
+
+export const Category = styled.h5`
+  color: ${colors.white};
+  padding: 10px;
+  border-radius: 5px;
+  background-color: ${colors.mediumDarkPurple};
+  margin: 0;
+  font-size: 18px;
+  cursor: pointer;
+`;
+
+export const SectionHeader = styled.h2`
+  margin: 0;
+  color: ${colors.orange};
+`;

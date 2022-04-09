57cfa2813b26e358e62310e117d4025085b90995
Ben <benmotykax@gmail.com>
2022-04-09 23:51:09 +0200

    fixed responsivity for faq

--- src/components/Faq/Faq.styles.js
+++ src/components/Faq/Faq.styles.js
@@ -10,28 +10,45 @@ export const Container = styled.div`
   padding: 100px 0;
   box-sizing: border-box;
   border-radius: 5px;
+  min-width: 800px;
 
   @media screen and (max-width: 1000px) {
+    min-width: 200px;
     padding: 50px 0;
   }
 `;
 
 export const HeaderWrapper = styled.div`
-  width: 40%;
+  width: 50%;
   text-align: center;
+  
+  @media screen and (max-width: 1000px) {
+    width: 80%;
+  }
 `;
 
 export const Header = styled.h1`
   font-size: 32px;
   color: ${colors.white};
   margin: 0;
+  @media screen and (max-width: 1000px) {
+    font-size: 24px;
+  }
 `;
 
 export const CategoriesWrapper = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
   width: 50%;
-  column-gap: 20px;
+  gap: 20px;
+  @media screen and (max-width: 1000px) {
+    width: 80%;
+  }
+
+  @media screen and (max-width: 450px) {
+    grid-template-columns: 1fr;
+    grid-template-rows: 1fr 1fr 1fr;
+  }
 `;
 
 export const Category = styled.div`
@@ -55,16 +72,25 @@ export const Category = styled.div`
     opacity: 1;
     border: 2px solid ${colors.orange};
   }
+
+  @media screen and (max-width: 1000px) {
+    padding: 15px;
+    font-size: 18px;
+  }
 `;
 
 export const ItemsWrapper = styled.ul`
-  background-color:  ${colors.mediumDarkPurple};
+  background-color: ${colors.mediumDarkPurple};
   width: 50%;
   list-style: none;
   border-radius: 6px;
   display: flex;
   flex-direction: column;
   padding: 0;
+
+  @media screen and (max-width: 1000px) {
+    width: 80%;
+  }
 `;
 
 export const Item = styled.li`
@@ -74,19 +100,25 @@ export const Item = styled.li`
   margin: 20px;
   margin-bottom: 0;
   &:last-child {
-  margin-bottom: 20px;
+    margin-bottom: 20px;
   }
 `;
 
 export const Question = styled.h3`
-margin: 0 0 10px 0;
-font-size: 20px;
-`
+  margin: 0 0 10px 0;
+  font-size: 20px;
+
+  @media screen and (max-width: 800px) {
+    font-size: 16px;
+  }
+`;
 
 export const Answer = styled.p`
-margin: 0;
-
-`
+  margin: 0;
+  @media screen and (max-width: 800px) {
+    font-size: 14px;
+  }
+`;
 
 export const FaqInput = styled.input`
   font-size: 18px;
@@ -103,10 +135,14 @@ export const FaqInput = styled.input`
 `;
 
 export const Text = styled.p`
-font-size: 20px;
-color: ${colors.white};
-`
+  font-size: 20px;
+  color: ${colors.white};
+
+  @media screen and (max-width: 800px) {
+    font-size: 16px;
+  }
+`;
 
 export const Highlight = styled.span`
-color: ${colors.orange};
-`
+  color: ${colors.orange};
+`;

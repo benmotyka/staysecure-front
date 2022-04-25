ec03a995ec2d6017cf104beac0e3eacc9ea0ee61
ben <benmotykax@gmail.com>
2022-04-25 21:46:53 +0200

    added recoil, added user atom

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import Notebook from "components/Notebook/Notebook";
 import { withRouter } from "react-router";
 import Routes from "routes";
+import { RecoilRoot } from 'recoil';
 
-import 'i18n.js'
+import "i18n.js";
 import { useEffect } from "react";
 import { useTranslation } from "react-i18next";
 
 function App() {
-  const {i18n} = useTranslation()
+  const { i18n } = useTranslation();
 
   useEffect(() => {
-    if(i18n.language === 'en') document.title = 'Stay Secure - cybersecurity courses'
-  },[])
+    if (i18n.language === "en")
+      document.title = "Stay Secure - cybersecurity courses";
+  }, []);
   return (
-    <>
-    <Routes/>
-    <Notebook/>
-    </>
+    <RecoilRoot>
+      <Routes />
+      <Notebook />
+    </RecoilRoot>
   );
 }
 

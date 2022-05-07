fa68003f86fcb8a422b2eda9132c0702cdbf441f
ben <benmotykax@gmail.com>
2022-05-07 19:52:18 +0200

    changed way of loading preview items

--- src/pages/courses.js
+++ src/pages/courses.js
@@ -1,18 +1,18 @@
 import Navbar from 'components/Navbar/Navbar'
 import Footer from 'components/Footer/Footer'
-import {PageWrapper} from 'components/Pages/Pages.styles'
+import {PageWrapperNoCentered} from 'components/Pages/Pages.styles'
 import CoursesPreview from 'components/PreviewItems/Courses'
 import { useTranslation } from 'react-i18next'
 
 const Courses = () => {
-  const {t, i18n} = useTranslation()
+  const {t} = useTranslation()
 
     return (
         <>
             <Navbar/>
-            <PageWrapper>
+            <PageWrapperNoCentered>
             <CoursesPreview header={t('allCourses')} />
-            </PageWrapper>
+            </PageWrapperNoCentered>
             <Footer/>
         </>
     )

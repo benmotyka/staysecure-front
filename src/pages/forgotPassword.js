fe2ca5df127e81efb5c0bd3e4c8e222fa4b38da4
Ben <benmotykax@gmail.com>
2022-06-05 16:40:15 +0200

    added titles to all pages

--- src/pages/forgotPassword.js
+++ src/pages/forgotPassword.js
@@ -1,17 +1,25 @@
-import Footer from "components/Footer/Footer"
-import Navbar from "components/Navbar/Navbar"
-import { PageOneItemCentered } from "components/Pages/Pages.styles"
-import ForgotPasswordWidget from "components/Cards/ForgotPassword"
+import Footer from "components/Footer/Footer";
+import Navbar from "components/Navbar/Navbar";
+import { PageOneItemCentered } from "components/Pages/Pages.styles";
+import ForgotPasswordWidget from "components/Cards/ForgotPassword";
+import { Helmet } from "react-helmet";
+import { useTranslation } from "react-i18next";
+
 const ForgotPassword = () => {
+  const { t } = useTranslation();
+
   return (
     <>
-    <Navbar/>
-    <PageOneItemCentered>
-        <ForgotPasswordWidget/>
-    </PageOneItemCentered>
-    <Footer/>
+      <Helmet>
+        <title>{t("helmet.titles.login")}</title>
+      </Helmet>
+      <Navbar />
+      <PageOneItemCentered>
+        <ForgotPasswordWidget />
+      </PageOneItemCentered>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default ForgotPassword
+export default ForgotPassword;
--- src/pages/forgotPasswordChange.js
+++ src/pages/forgotPasswordChange.js
@@ -1,18 +1,26 @@
-import Footer from "components/Footer/Footer"
-import Navbar from "components/Navbar/Navbar"
-import { PageOneItemCentered } from "components/Pages/Pages.styles"
-import ForgotPasswordChangeWidget from "components/Cards/ForgotPasswordChange"
+import Footer from "components/Footer/Footer";
+import Navbar from "components/Navbar/Navbar";
+import { PageOneItemCentered } from "components/Pages/Pages.styles";
+import ForgotPasswordChangeWidget from "components/Cards/ForgotPasswordChange";
+import { Helmet } from "react-helmet";
+import { useTranslation } from "react-i18next";
+
 const ForgotPasswordChange = (props) => {
+  const { t } = useTranslation();
   const token = props.match.params.token;
+  
   return (
     <>
-    <Navbar/>
-    <PageOneItemCentered>
-        <ForgotPasswordChangeWidget token={token}/>
-    </PageOneItemCentered>
-    <Footer/>
+      <Helmet>
+        <title>{t("helmet.titles.login")}</title>
+      </Helmet>
+      <Navbar />
+      <PageOneItemCentered>
+        <ForgotPasswordChangeWidget token={token} />
+      </PageOneItemCentered>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default ForgotPasswordChange
+export default ForgotPasswordChange;

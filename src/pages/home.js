29eb66c8bfc5123d98c4752f0f7ef3075e9ee6c0
ben <benmotykax@gmail.com>
2022-12-11 18:50:40 +0100

    refactored routes file

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,31 +1,30 @@
-import React from 'react'
-
-import Navbar from 'components/Navbar/Navbar'
-import Hero from 'components/Hero/Hero'
-import Footer from 'components/Footer/Footer'
-import {PageWrapper} from 'components/Pages/Pages.styles'
-import Articles from 'components/PreviewItems/Articles'
-import Courses from 'components/PreviewItems/Courses'
+import React from "react";
+import Navbar from "components/Navbar/Navbar";
+import Hero from "components/Hero/Hero";
+import Footer from "components/Footer/Footer";
+import { PageWrapper } from "components/Pages/Pages.styles";
+import Articles from "components/PreviewItems/Articles";
+import Courses from "components/PreviewItems/Courses";
 import { useTranslation } from "react-i18next";
-import { Helmet } from 'react-helmet'
+import { Helmet } from "react-helmet";
 
 const Home = () => {
-  const {t} = useTranslation()
+  const { t } = useTranslation();
 
-    return (
-        <>
-            <Helmet>
-                <title>{t('helmet.titles.home')}</title>
-            </Helmet>
-            <Navbar/>
-            <Hero/>
-            <PageWrapper>
-            <Courses header={t('lastCourses')} quantity={3}/>
-            <Articles header={t('lastArticles')} quantity={6}/>
-            </PageWrapper>
-            <Footer/>
-        </>
-    )
-}
+  return (
+    <>
+      <Helmet>
+        <title>{t("helmet.titles.home")}</title>
+      </Helmet>
+      <Navbar />
+      <Hero />
+      <PageWrapper>
+        <Courses header={t("lastCourses")} quantity={3} />
+        <Articles header={t("lastArticles")} quantity={6} />
+      </PageWrapper>
+      <Footer />
+    </>
+  );
+};
 
-export default Home
+export default Home;
